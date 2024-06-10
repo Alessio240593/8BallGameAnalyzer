253b5c317def3d7efa8fa8cada6aebfacdc4a968
Alessio240593 <alessiozattoni93@gmail.com>
2024-06-10 15:52:24 +0200

    improve ball detector

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-std::string classifyBall(cv::Mat& img, cv::Point center, int radius)
+enum class Classification {
+    CUE_BALL,
+    EIGHT_BALL,
+    WHITE_STRIPES,
+    SOLID_COLOR
+};
+
+struct Ball {
+    cv::Point center;
+    float radius;
+    Classification classification;
+};
+
+Classification classifyBall(cv::Mat& img, cv::Point center, float radius, std::vector<Ball>& balls)
 {
     int x = center.x - radius;
     int y = center.y - radius;
@@ -18,29 +31,39 @@ std::string classifyBall(cv::Mat& img, cv::Point center, int radius)
     cv::Mat roi_gray;
     cv::cvtColor(roi, roi_gray, cv::COLOR_BGR2GRAY);
 
+    cv::Mat filtered;
+    cv::GaussianBlur(roi_gray, filtered, cv::Size(3, 3), 3);
+
     cv::Mat mask_white;
-    cv::threshold(roi_gray, mask_white, 200, 255, cv::THRESH_BINARY);
+    cv::inRange(filtered, cv::Scalar(180), cv::Scalar(255), mask_white);
 
     cv::Mat mask_black;
-    cv::threshold(roi_gray, mask_black, 50, 255, cv::THRESH_BINARY_INV);
+    cv::inRange(filtered, cv::Scalar(0), cv::Scalar(50), mask_black);
 
     int white_pixel_count = cv::countNonZero(mask_white);
     int black_pixel_count = cv::countNonZero(mask_black);
+    int total_pixels = width * height;
 
-    if (white_pixel_count > 0.30 * width * height)
-        return "Cue ball";
-    else if (black_pixel_count > 0.30 * width * height)
-        return "8 ball";
-    else if (white_pixel_count > 0.05 * width * height)
-        return "With stripes";
+    Classification classification;
+
+    if (white_pixel_count > 0.30 * total_pixels)
+        classification =  Classification::CUE_BALL;
+    else if (black_pixel_count > 0.30 * total_pixels)
+        classification = Classification::EIGHT_BALL;
+    else if (white_pixel_count > 0.01 * total_pixels)
+        classification = Classification::WHITE_STRIPES;
     else
-        return "Solid color";
+        classification = Classification::SOLID_COLOR;
+
+    balls.emplace_back(Ball{center, radius, classification});
+
+    return classification;
 }
 
 cv::Point2f computeIntersection(cv::Vec4i a, cv::Vec4i b)
 {
     cv::Point2f p(-1, -1);
-    float denominator = static_cast<float>((a[0] - a[2]) * (b[1] - b[3]) - (a[1] - a[3]) * (b[0] - b[2]));
+    auto denominator = static_cast<float>((a[0] - a[2]) * (b[1] - b[3]) - (a[1] - a[3]) * (b[0] - b[2]));
     if (denominator != 0) {
         p.x = static_cast<float>((a[0] * a[3] - a[1] * a[2]) * (b[0] - b[2]) - (a[0] - a[2]) * (b[0] * b[3] - b[1] * b[2])) / denominator;
         p.y = static_cast<float>((a[0] * a[3] - a[1] * a[2]) * (b[1] - b[3]) - (a[1] - a[3]) * (b[0] * b[3] - b[1] * b[2])) / denominator;
@@ -48,28 +71,52 @@ cv::Point2f computeIntersection(cv::Vec4i a, cv::Vec4i b)
     return p;
 }
 
-cv::Scalar getColor(std::string classification)
+cv::Scalar getColor(Classification classification)
 {
-    if (classification == "Cue ball")
-        return {255, 255, 255};
-    else if (classification == "8 ball")
-        return {0, 0, 0};
-    else if (classification == "With stripes")
-        return {0, 255, 0};
-    else if (classification == "Solid color")
-        return {255, 0, 0};
-    else
-        return {255, 0, 255};
+    switch (classification) {
+        case Classification::CUE_BALL: return {255, 255, 255};
+        case Classification::EIGHT_BALL: return {0, 0, 0};
+        case Classification::WHITE_STRIPES: return {255, 178, 102};
+        case Classification::SOLID_COLOR: return {0, 0, 255};
+        default: return {0, 0, 0};
+    }
 }
 
-int main()
+void drawFieldAndBalls(cv::Mat& image, const std::vector<std::vector<cv::Point>>& field, const std::vector<Ball>& balls, const cv::Rect& boundingBox)
 {
-    cv::Mat image = cv::imread("../Images/frame_first2.png");
-    if (image.empty()) {
-        std::cerr << "Could not load the image" << std::endl;
-        return -1;
-    }
+    cv::fillPoly(image, field, cv::Scalar(0, 100, 0));
 
+    for (const auto& ball : balls) {
+        cv::Point originalCenter(ball.center.x + boundingBox.x, ball.center.y + boundingBox.y);
+        int radius = ball.radius;
+        Classification classification = ball.classification;
+        cv::Scalar color = getColor(classification);
+        cv::circle(image, originalCenter, radius, color, -1);
+    }
+}
+
+void createMinimap(cv::Mat image, std::vector<std::vector<cv::Point>> hulls, std::vector<Ball> balls, cv::Rect boundingBox) {
+    int frameHeight = image.rows;
+    int frameWidth = image.cols;
+
+    cv::Mat miniMap = cv::Mat::zeros(cv::Size(frameWidth, frameHeight), CV_8UC3);
+
+    drawFieldAndBalls(miniMap, hulls, balls, boundingBox);
+
+    cv::Mat resizedMiniMap;
+    cv::resize(miniMap, resizedMiniMap, cv::Size(), 0.25, 0.25);
+
+    int miniMapHeight = resizedMiniMap.rows;
+    int miniMapWidth = resizedMiniMap.cols;
+
+    cv::Rect miniMapRect(0, frameHeight - miniMapHeight, miniMapWidth, miniMapHeight);
+    cv::Mat miniMapRegion = image(miniMapRect);
+    resizedMiniMap.copyTo(miniMapRegion);
+}
+
+void processFrame(cv::Mat image)
+{
+    cv::Mat img = image.clone();
     int xOffset = image.cols * 0.13;
     int yOffset = image.rows * 0.13;
     int cropWidth = image.cols - 2 * xOffset;
@@ -91,11 +138,10 @@ int main()
     cv::HoughLinesP(edges, lines, 1, CV_PI / 180, 100, 50, 50);
 
     std::vector<cv::Vec4i> filteredLines;
-    for (size_t i = 0; i < lines.size(); i++) {
-        cv::Vec4i line = lines[i];
+    for (auto line : lines) {
         double length = sqrt(pow(line[2] - line[0], 2) + pow(line[3] - line[1], 2));
 
-        if (length > 200)
+        if (length > 175)
             filteredLines.push_back(line);
     }
 
@@ -131,44 +177,93 @@ int main()
         cv::polylines(image, hulls, true, borderColor, thickness);
 
         cv::Mat mask = cv::Mat::zeros(image.size(), CV_8UC1);
-
         cv::fillPoly(mask, hulls, cv::Scalar(255));
 
-        cv::Mat result;
+        cv::Mat result = cv::Mat::zeros(image.size(), image.type());
         image.copyTo(result, mask);
 
         cv::Rect boundingBox = cv::boundingRect(hull);
-
         cv::Mat croppedResult = result(boundingBox);
 
         cv::Mat img_gray;
         cv::cvtColor(croppedResult, img_gray, cv::COLOR_BGR2GRAY);
 
-        cv::medianBlur(img_gray, img_gray, 5);
+        cv::Mat croppedFiltered;
+        cv::bilateralFilter(img_gray, croppedFiltered, 5, 75, 75);
 
-        cv::equalizeHist(img_gray, img_gray);
+        cv::Mat inverted;
+        cv::bitwise_not(croppedFiltered, inverted);
 
-        std::vector<cv::Vec3f> circles;
-        cv::HoughCircles(img_gray, circles, cv::HOUGH_GRADIENT, 1, img_gray.rows / 16, 100, 10, 8, 15);
+        cv::Mat binarized;
+        cv::threshold(inverted, binarized, 160, 255, cv::THRESH_BINARY );
 
-        if (!circles.empty()) {
-            for (size_t i = 0; i < circles.size(); i++) {
-                cv::Vec3i c = circles[i];
-                cv::Point center = cv::Point(c[0], c[1]);
-                int radius = c[2];
+        cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(7, 7));
+        cv::Mat morphed;
+        cv::morphologyEx(binarized, morphed, cv::MORPH_OPEN, element);
 
-                std::string classification = classifyBall(croppedResult, center, radius);
+        cv::Mat element2 = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5));
+        cv::Mat dilatate;
+        cv::dilate(morphed, dilatate, element2);
+
+        std::vector<std::vector<cv::Point>> contours;
+        cv::findContours(dilatate, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+        std::vector<Ball> balls;
+        for (const auto& contour : contours) {
+            cv::Point2f center;
+            float radius;
+            cv::minEnclosingCircle(contour, center, radius);
+
+            if (radius > 10 && radius < 50) {
+                cv::Point originalCenter(center.x + boundingBox.x, center.y + boundingBox.y);
+
+                Classification classification = classifyBall(croppedResult, center, radius, balls);
                 cv::Scalar color = getColor(classification);
 
-                cv::Point originalCenter(center.x + boundingBox.x, center.y + boundingBox.y);
                 cv::rectangle(image, cv::Rect(originalCenter.x - radius, originalCenter.y - radius, radius * 2, radius * 2), color, 2);
-                cv::putText(image, classification, cv::Point(originalCenter.x - radius, originalCenter.y - radius - 10), cv::FONT_HERSHEY_TRIPLEX, 0.3, color, 1);
             }
+        }
+
+        createMinimap(image, hulls, balls, boundingBox);
+
+        cv::imshow("Billiard Video Analysis", image);
+    }
+}
+
+int main()
+{
+    cv::VideoCapture cap;
+    cap.open("../Images/game1_clip2.mp4");
+
+    if (!cap.isOpened()) {
+        std::cout << "Error: Unable to open video file" << std::endl;
+        return -1;
+    }
+
+    double dWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+    double dHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    std::cout << "Resolution of the video: " << dWidth << " x " << dHeight << std::endl;
+
+    std::string window_name = "Billiard Video Analysis";
+    namedWindow(window_name, cv::WINDOW_NORMAL);
+
+    cv::Mat frame;
+    while (true) {
+        bool bSuccess = cap.read(frame);
+        if (!bSuccess) {
+            std::cout << "Video is completed" << std::endl;
+            break;
+        }
+
+        processFrame(frame);
+
+        if (cv::waitKey(10) == 27) {
+            std::cout << "Esc key is pressed by user. Stopping the video" << std::endl;
+            break;
         }
     }
 
-    cv::imshow("Billiard Table with Border", image);
-    cv::waitKey(0);
-
+    cap.release();
+    cv::destroyAllWindows();
     return 0;
 }
